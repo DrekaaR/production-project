69552774d57cecd66fb3e832c17e2c0a2d9b0034
Alex <40631201+DrekaaR@users.noreply.github.com>
2023-06-10 12:49:51 +0200

    fix dev plugins

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -18,15 +18,16 @@ export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPlu
         new webpack.DefinePlugin({
             __IS_DEV__: JSON.stringify(isDev),
         }),
-        new BundleAnalyzerPlugin({
-            openAnalyzer: false,
-        }),
+
     ];
 
     if (isDev) {
         plugins.push(
             new webpack.HotModuleReplacementPlugin(),
             new ReactRefreshPlugin(),
+            new BundleAnalyzerPlugin({
+                openAnalyzer: false,
+            }),
         );
     }
 
